39e01e1306d59f476bdd5a6ba80793a192d0d918
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-28 12:21:34 +0100

    Add favourite button

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -4,6 +4,9 @@ import "./ImagePreview.css";
 export default function ImagePreview({ src, alt, author }) {
   return (
     <div className="imageContainer">
+      <button class="favourite-btn" onClick={() => alert("Clicked!")}>
+        ❤
+      </button>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
